81251aa81078d344a1fa2226511d8c0805fbe8c3
Alexander Karlsson <alex94@comhem.se>
2015-06-03 15:34:33 +0200

    Fixed bugs and added GUI

--- Kortmaskinen/Model/PlayingCardDeck.m
+++ Kortmaskinen/Model/PlayingCardDeck.m
@@ -13,17 +13,19 @@
 
 -(instancetype)init
 {
-    [self init]
+    self = [super init];
     
     if(self){
         for(NSString *suit in [PlayingCard validSuits]){
             for(NSUInteger rank=1; rank<=[PlayingCard maxRank]; rank++){
-                PlayingCard *card = [[PlayingCard alloc] init]
+                PlayingCard *card = [[PlayingCard alloc] init];
                 card.suit = suit;
                 card.rank = rank;
                 [self addCard:card];
             }
         }
     }
+    
+    return self;
 }
 @end
--- Kortmaskinen/ViewController.m
+++ Kortmaskinen/ViewController.m
@@ -7,21 +7,31 @@
 //
 
 #import "ViewController.h"
+#import "PlayingCardDeck.h"
 
 @interface ViewController ()
-
+@property (strong, nonatomic) PlayingCardDeck *deck;
 @end
 
 @implementation ViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (IBAction)cardPressed:(UIButton *)sender {
+    if([[sender currentTitle] length]){
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardBack"]
+                          forState:UIControlStateNormal];
+        [sender setTitle:nil forState:UIControlStateNormal];
+    }else{
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardFront"]
+                          forState: UIControlStateNormal];
+        NSString *cardString = [[self.deck drawRandomCard] contents];
+        [sender setTitle:cardString forState:UIControlStateNormal];
+    }
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(PlayingCardDeck *)deck
+{
+    if(!_deck){
+        _deck = [[PlayingCardDeck alloc]init];
+    }
+    return _deck;
 }
-
 @end
